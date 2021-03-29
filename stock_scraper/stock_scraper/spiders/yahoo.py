07a76a0486c2a13bb0cb38f0158e313a2b618465
arsoljon <jonathan.arsola01@utrgv.edu>
2021-03-29 14:34:18 -0500

    Inside the YahooSpider class, I extracted the ticker form 'ticker_url_db' and made the search for the current price then saved into 'clean_stock_db'.

--- stock_scraper/stock_scraper/spiders/yahoo.py
+++ stock_scraper/stock_scraper/spiders/yahoo.py
@@ -1,10 +1,23 @@
 import scrapy
-
+import pandas as pd
+import os
 
 class YahooSpider(scrapy.Spider):
+    ticker = ''
+    df_ticker = pd.read_csv('../ticker_url_db', sep=',', dtype=str)
+    ticker = df_ticker.to_dict()["Ticker"][0]
+    site_loc = "quote/"
+
     name = 'yahoo'
     allowed_domains = ['finance.yahoo.com']
-    start_urls = ['http://finance.yahoo.com/']
+    start_urls = ['http://finance.yahoo.com/'+site_loc+ticker]
 
     def parse(self, response):
-        pass
+        price = response.xpath('//*[@class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"]/text()').extract()[0]
+        data = {"Ticker": [self.ticker], "Current_price": [price]}
+        df_data = pd.DataFrame(data, columns=["Ticker", "Current_price"])
+        yield{df_data.to_csv("clean_stock_db", sep=',')}
+        yield{"Ticker": self.ticker, "Current_price": price}
+
+#<span class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid="50">1.13</span>
+
