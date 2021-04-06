745a8c7a02c4412f5e1ec9f64c3d2e6740ebd97c
arsoljon <jonathan.arsola01@utrgv.edu>
2021-04-06 16:22:12 -0500

    attempted to make a test to check 'get_url()' returns the url saved in the setter function.

--- all_tests/search/search_test.py
+++ all_tests/search/search_test.py
@@ -1,9 +1,11 @@
 import unittest
-from search_stock import search
+from search_stock import search as s
 
 class SearchTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_url(self):
+        url = "https://finance.yahoo.com/quote/"
+        a = s.Search('sndl')
+        self.assertEqual(a.get_url(), url)
 
 
 if __name__ == '__main__':
