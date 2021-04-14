682bb60e6ec4c2ac3a33d8eb80e0311acc5bcad2
arsoljon <jonathan.arsola01@utrgv.edu>
2021-04-14 16:22:14 -0500

    .

--- all_tests/search/search_test.py
+++ all_tests/search/search_test.py
@@ -2,10 +2,9 @@ import unittest
 from search_stock import search as s
 
 class SearchTest(unittest.TestCase):
-    def test_url(self):
+    def test_get_url(self):
         url = "https://finance.yahoo.com/quote/"
-        a = s.Search('sndl')
-        self.assertEqual(a.get_url(), url)
+        self.assertEqual(s.Search('sndl').get_url(), url)
 
 
 if __name__ == '__main__':
